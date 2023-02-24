013479f3f10c0e653b46a9e6d123e5000d91cfb2
yusikerem <kerem.yky@gmail.com>
2023-02-24 22:29:50 +0300

    create project and add styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <pagination-vue> </pagination-vue>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PaginationVue from "./components/PaginationVue.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    PaginationVue,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body * {
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/PaginationVue.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="container">
+    <button disabled class="button-disabled">&lt;</button>
+    <button class="page page-active">1</button>
+
+    <button class="page page-disabled">2</button>
+    <button class="button-active">&gt;</button>
+  </section>
+</template>
+<script></script>
+<style>
+:root {
+  --color-page-active: #4200ff;
+  --color-primary: #c4cdd5;
+  --bg: #dfe3e8;
+  --bg-primary: white;
+  --bg-button-disabled: #919eab;
+}
+.container {
+  padding: 10rem;
+
+  width: 100%;
+  background-color: var(--bg);
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+}
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 2rem;
+  min-height: 2rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 150ms;
+}
+button:not(.button-disabled):hover {
+  transform: scale(1.05);
+}
+.button-active {
+  background: var(--bg-primary);
+  color: var(--color-primary);
+  font-size: x-large;
+}
+.button-disabled {
+  background: var(--bg-button-disabled);
+  color: var(--color-primary);
+  font-size: x-large;
+  cursor: default;
+}
+.page {
+  font-weight: bold;
+  background: var(--bg-primary);
+}
+.page-active {
+  border-color: var(--color-page-active);
+  color: var(--color-page-active);
+  border: solid 1px;
+}
+.page-disabled {
+  background-color: var(--bg-primary);
+}
+img {
+  box-shadow: none;
+}
+</style>
