c0368794faa5fcfac9633352709d6559002a8739
yusikerem <kerem.yky@gmail.com>
2023-02-25 11:17:59 +0300

    pagination components work with local data

--- src/components/PaginationVue.vue
+++ src/components/PaginationVue.vue
@@ -1,13 +1,71 @@
 <template>
   <section class="container">
-    <button disabled class="button-disabled">&lt;</button>
-    <button class="page page-active">1</button>
+    <button
+      :disabled="activeIndex === 0 ? true : false"
+      :class="{ 'button-disabled': activeIndex === 0 }"
+      @click="activeIndex--"
+      class="button-active"
+    >
+      &lt;
+    </button>
 
-    <button class="page page-disabled">2</button>
-    <button class="button-active">&gt;</button>
+    <component
+      :class="[
+        page == '...' ? 'dots' : 'page',
+        { 'page-active': activeIndex + 1 === page },
+      ]"
+      class="page-disabled"
+      @click="page === '...' ? undefined : (activeIndex = page - 1)"
+      :key="page"
+      v-for="page in displayPages()"
+      :is="page == '...' ? 'div' : 'button'"
+      >{{ page }}</component
+    >
+
+    <button
+      :disabled="activeIndex + 1 === totalPage ? true : false"
+      :class="{ 'button-disabled': activeIndex + 1 === totalPage }"
+      @click="activeIndex++"
+      class="button-active"
+    >
+      &gt;
+    </button>
   </section>
 </template>
-<script></script>
+<script setup>
+import { ref } from "vue";
+const activeIndex = ref(0);
+const itemCount = 60;
+const itemCountPerPage = 5;
+const totalPage = Math.round(Math.floor(itemCount / itemCountPerPage));
+const maxPageCount = 7;
+
+const displayPages = function () {
+  return totalPage > maxPageCount
+    ? activeIndex.value < 4
+      ? [1, 2, 3, 4, 5, "...", totalPage]
+      : activeIndex.value > totalPage - 5
+      ? [
+          1,
+          "...",
+          totalPage - 4,
+          totalPage - 3,
+          totalPage - 2,
+          totalPage - 1,
+          totalPage,
+        ]
+      : [
+          1,
+          "...",
+          activeIndex.value,
+          activeIndex.value + 1,
+          activeIndex.value + 2,
+          "...",
+          totalPage,
+        ]
+    : totalPage;
+};
+</script>
 <style>
 :root {
   --color-page-active: #4200ff;
@@ -38,6 +96,15 @@ button {
   transition-property: all;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 150ms;
+}
+.dots {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 2rem;
+  min-height: 2rem;
+  border: none;
+  border-radius: 4px;
 }
 button:not(.button-disabled):hover {
   transform: scale(1.05);
